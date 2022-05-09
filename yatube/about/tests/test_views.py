397aeb0590ef3b86714f70cc68cf958039c67ced
Marik63 <94235725+Marik63@users.noreply.github.com>
2022-05-09 15:03:54 +0300

    Отправил задание на проверку

--- yatube/about/tests/test_views.py
+++ yatube/about/tests/test_views.py
@@ -1,4 +1,6 @@
-﻿from django.test import Client, TestCase
+from http import HTTPStatus
+
+from django.test import TestCase, Client
 from django.urls import reverse
 
 
@@ -6,13 +8,24 @@ class StaticViewsTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
 
-    def test_about_page_accessible_by_name(self):
-        """URL, генерируемый при помощи имени static_pages:author, доступен."""
-        response = self.guest_client.get(reverse('static_pages:author'))
-        self.assertEqual(response.status_code, 200)
+    def test_about_pages_accessible_by_name(self):
+        """URLs, приложения about доступны."""
+        urls = {
+            reverse('about:author'): HTTPStatus.OK,
+            reverse('about:tech'): HTTPStatus.OK,
+        }
+        for url, expected_status in urls.items():
+            with self.subTest(url=url):
+                response = self.guest_client.get(url)
+                self.assertEqual(response.status_code, expected_status)
 
-    def test_about_page_uses_correct_template(self):
-        """При запросе к staticpages:author
-        применяется шаблон staticpages/author.html."""
-        response = self.guest_client.get(reverse('static_pages:author'))
-        self.assertTemplateUsed(response, 'static_pages/author.html')
+    def test_about_pages_uses_correct_template(self):
+        """При запросе к about применяется правильные шаблоны"""
+        urls = {
+            reverse('about:author'): 'about/author.html',
+            reverse('about:tech'): 'about/tech.html',
+        }
+        for url, expected_template in urls.items():
+            with self.subTest(url=url):
+                response = self.guest_client.get(url)
+                self.assertTemplateUsed(response, expected_template)
